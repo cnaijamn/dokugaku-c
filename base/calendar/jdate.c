8df7774438f75be5248bbdab91018d184b75fdd8
Chez Naijamn <cnaijamn@gmail.com>
2026-03-09 13:35:41 +0900

    追加

---
+++ base/calendar/jdate.c
@@ -0,0 +1,27 @@
+// jdate / ndaysj : ユリウス暦(Julian)
+// ユリウス通日(天文学用、JD、Julian Day)とは違うので注意！
+#include <calendar.h>
+#include <stdio.h>
+
+int
+main(void)
+{
+	int n;
+	struct date dt;
+
+	// B.C.1年(西暦0年)3月1日を起点(0)として、
+	// 指定された日までの経過日数を返す
+	n = ndaysj(&(struct date){
+	    .y = 0,
+	    .m = 3,
+	    .d = 1,
+	});
+	printf("ndaysj -> %d\n", n);
+	// ndaysj -> 0
+
+	(void)jdate(n, &dt);
+	printf("jdate -> %04d-%02d-%02d\n", dt.y, dt.m, dt.d);
+	// jdate -> 0000-03-01
+
+	return (0);
+}
